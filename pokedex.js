a4c2855271ad9a2d7b6f8c85b50d084f97bb5769
akash-panthri <akashpanthri2000@gmail.com>
2023-12-22 17:57:34 +0530

    Initial commit

---
+++ pokedex.js
@@ -0,0 +1,49 @@
+const pokeconstainer = document.getElementById('poke-container')
+
+const pokemon_count  = 50
+
+const colos = {
+
+}
+
+const fetchPokemons = async ()=>{
+    for(let i = 1; i<=pokemon_count;i++){
+await getPokemon(i)
+    }
+}
+
+const getPokemon = async(id)=>{
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}`
+
+    const res = await fetch(url)
+    const data = await res.json()
+createPokemonCard(data)
+}
+
+fetchPokemons()
+
+const createPokemonCard =(pokemon)=>{
+    const pokemonEl = document.createElement('div')
+    pokemonEl.classList.add('pokemon')
+
+
+    const pokemonInnerHTML =`
+    <div class="pokemon" style="background-color: rgb(222, 253, 224)">
+        <div class="img-container">
+          <img
+            src=https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png"" alt="${name}"
+          />
+        </div>
+        <div class="info">
+          <span class="number"></span>
+          <h3 class="name">${pokemon.name}</h3>
+          
+        </div>
+      </div>
+      `
+
+    pokemonEl.innerHTML = pokemonInnerHTML
+
+    pokeconstainer.appendChild(pokemonEl)
+
+}
